af2f629eb321032c19d60fdfbb91c4652ae5b56c
LionelYatesJr <46111161+LionelYatesJr@users.noreply.github.com>
2024-04-06 14:13:15 -0400

    book.ts

--- server/models/book.ts
+++ server/models/book.ts
@@ -1,10 +1,10 @@
 export interface Book {
   id: number;
-  title: string; // Renamed from 'name' to 'title'
+  title: string; 
   author: string;
   genre: string;
   availability: 'Available' | 'Checked Out' | 'Out of Stock'; // Union type for availability
-  waitlist?: string[]; // Optional array of users on waitlist
+  waitlist?: string[]; 
   numberOfCopies: number;
 }
 
